366daece2a5ebf98d4d9f326d41f440a42c9f348
nunziati <giacomo.nunziati.0@gmail.com>
2022-03-18 15:20:45 +0100

    validation set treated separately

---
+++ preprocess.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    # Load ids files
+    test_story_ids = pd.read_csv("./dataset/test_story_ids.csv")
+    train_story_ids = pd.read_csv("./dataset/train_story_ids.csv")
+    val_story_ids = pd.read_csv("./dataset/dev_story_ids.csv")
+
+    # Load data already tokenized "by using ws as token separator"
+    news_qa_data_tokenized = pd.read_csv(
+        "./dataset/newsqa-data-tokenized-v1.csv", sep=",")
+    news_qa_data_tokenized.question = news_qa_data_tokenized.question.fillna(
+        "")
+    
+    # Drop useless columns
+    news_qa_data_tokenized = news_qa_data_tokenized.drop(
+        columns=["sentence_starts", "answer_char_ranges"])
+    
+    # Split data
+    news_qa_train = pd.merge(
+        train_story_ids, news_qa_data_tokenized, how="inner", on="story_id")
+    news_qa_train.is_answer_absent = news_qa_train.is_answer_absent.replace("?", "0.5")
+    news_qa_train.is_question_bad = news_qa_train.is_question_bad.replace("?", "0.5")
+    news_qa_train = news_qa_train.astype({"is_question_bad": np.float64})
+
+    news_qa_val = pd.merge(
+        val_story_ids, news_qa_data_tokenized, how="inner", on="story_id")
+    news_qa_val.is_answer_absent = news_qa_val.is_answer_absent.replace("?", "0.5")
+    news_qa_val.is_question_bad = news_qa_val.is_question_bad.replace("?", "0.5")
+    news_qa_val = news_qa_val.astype({"is_question_bad": np.float64})
+
+    news_qa_test = pd.merge(
+        test_story_ids, news_qa_data_tokenized, how="inner", on="story_id")
+    news_qa_test.is_answer_absent = news_qa_test.is_answer_absent = news_qa_test.is_answer_absent.replace(
+        "?", "0.5")
+    news_qa_test.is_question_bad = news_qa_test.is_question_bad = news_qa_test.is_question_bad.replace(
+        "?", "0.5")
+    news_qa_test = news_qa_test.astype({"is_question_bad": np.float64})
+    
+    
