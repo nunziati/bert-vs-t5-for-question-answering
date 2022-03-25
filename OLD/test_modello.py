03eb403723cd2a3a8bfaecbabde18e7cc7ecf576
Christian Di Maio <dimaio@kronos.diism.unisi.it>
2022-03-25 20:57:21 +0100

    First training version

--- OLD/test_modello.py
+++ OLD/test_modello.py
@@ -1,9 +1,9 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 tokenizer = T5Tokenizer.from_pretrained("t5-base")
-model = T5ForConditionalGeneration.from_pretrained("./models/gpu/checkpoint-5000")
+model = T5ForConditionalGeneration.from_pretrained("results/t5-base/model/checkpoint-0")
 
 context = """The Vatican Apostolic Library (), more commonly called the Vatican Library or simply the Vat, is the library of the Holy See, located in Vatican City. Formally established in 1475, although it is much older, it is one of the oldest libraries in the world and contains one of the most significant collections of historical texts. It has 75,000 codices from throughout history, as well as 1.1 million printed books, which include some 8,500 incunabula.   The Vatican Library is a research library for history, law, philosophy, science and theology. The Vatican Library is open to anyone who can document their qualifications and research needs. Photocopies for private study of pages from books published between 1801 and 1990 can be requested in person or by mail.   In March 2014, the Vatican Library began an initial four-year project of digitising its collection of manuscripts, to be made available online.   The Vatican Secret Archives were separated from the library at the beginning of the 17th century; they contain another 150,000 items.   Scholars have traditionally divided the history of the library into five periods, Pre-Lateran, Lateran, Avignon, Pre-Vatican and Vatican.   The Pre-Lateran period, comprising the initial days of the library, dated from the earliest days of the Church. Only a handful of volumes survive from this period, though some are very significant."""
-input_ids = tokenizer(f"question: what is the library for?  context: {context}", return_tensors="pt").input_ids
+input_ids = tokenizer(f"question: where is the Vatican City?  context: {context}", return_tensors="pt").input_ids
 outputs = model.generate(input_ids)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
--- training.py
+++ training.py
@@ -4,87 +4,105 @@ from datasets import load_dataset, load_metric
 from transformers import T5ForConditionalGeneration, T5Tokenizer, AdamW, set_seed
 from torch.utils.data import DataLoader
 
-f1_score = load_metric('f1')
+
 
 huggingface_model = 't5-base'
-batch_size = 32
+batch_size = 16
 seed = 7
 num_train_epochs = 40
-learning_rate = 1e-5
+learning_rate = 1e-4
 num_workers = 10
+max_input_length = 512
+
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, hf_dataset, tokenizer, truncate_strategy="truncate"):
         self.tokenizer = tokenizer
-        self.input = []
-        self.target = []
-
-        truncate = truncate_strategy == "truncate"
-
+        self.inputs_text = []
+        self.targets_text = []
+        
         for row in tqdm(hf_dataset):
-            encoded_input = tokenizer(f"question: {row['question']}  context: {row['plot']}",
-                return_tensors="pt", truncation=True, return_overflowing_tokens=True)
-            encoded_target = tokenizer(row['answers'][0] if len(row['answers']) > 0 else "",
-                return_tensors="pt", truncation=True, return_overflowing_tokens=True)
+            self.inputs_text.append(f"question: {row['question']}  context: {row['plot']}")
+            self.targets_text.append(row['answers'][0] if len(row['answers']) > 0 else "")
+        #     encoded_input = tokenizer(f"question: {row['question']}  context: {row['plot']}",
+        #         return_tensors="pt", truncation=True, return_overflowing_tokens=True)
+        #     encoded_target = tokenizer(row['answers'][0] if len(row['answers']) > 0 else "",
+        #         return_tensors="pt", truncation=True, return_overflowing_tokens=True)
 
-            if truncate_strategy == "truncate":
-                self.input.append(encoded_input.input_ids)
-                self.target.append(encoded_target.input_ids)
-            else:
-                if encoded_input['num_truncated_tokens'] + encoded_target['num_truncated_tokens'] == 0:
-                    self.input.append(encoded_input.input_ids)
-                    self.target.append(encoded_target.input_ids)
-                else:
-                    self.input.append(encoded_input['num_truncated_tokens'])
-                    self.target.append(encoded_target['num_truncated_tokens'])
+        #     if truncate_strategy == "truncate":
+        #         self.input.append(encoded_input.input_ids)
+        #         self.target.append(encoded_target.input_ids)
+        #     else:
+        #         if encoded_input['num_truncated_tokens'] + encoded_target['num_truncated_tokens'] == 0:
+        #             self.input.append(encoded_input.input_ids)
+        #             self.target.append(encoded_target.input_ids)
+        #         else:
+        #             self.input.append(encoded_input['num_truncated_tokens'])
+        #             self.target.append(encoded_target['num_truncated_tokens'])
 
-        if len(self.input) != len(self.target):
+        if len(self.inputs_text) != len(self.targets_text):
             raise Exception(
                 "something wrong while building the dataset: input and target result in different dimensions")
 
-        self.item_count = len(self.input)
+        self.item_count = len(self.inputs_text)
 
     def __len__(self):
         return self.item_count
 
     def __getitem__(self, index):
-        return self.input[index], self.target[index]
+        return self.inputs_text[index], self.targets_text[index]
         # QUI POTREMMO RITORNARE ANCHE LE attention_mask DELLE DUE STRINGHE TOKENIZZATE, CI SERVE?
 
-    def pack_minibatch(self, data):
-        input, target = zip(*data)
+    @staticmethod
+    def pack_minibatch(data):
+        inputs, targets = zip(*data)
+        encoded_inputs = tokenizer(
+                                inputs,
+                                padding="longest",
+                                max_length=max_input_length,
+                                truncation=True,
+                                return_tensors="pt",
+                            )
+        encoded_targets = tokenizer(
+                                targets,
+                                padding="longest",
+                                max_length=max_input_length,
+                                truncation=True,
+                                return_tensors="pt",
+                            )
         
-        input = torch.stack(input, 0)
-        target = torch.stack(target, 0)
+        input_ids, attention_mask = encoded_inputs.input_ids, encoded_inputs.attention_mask
+        encoded_targets = encoded_targets.input_ids
         
-        return input,target
+        # replace padding token id's of the labels by -100, crossEntropy skip target label == -100
+        encoded_targets[encoded_targets == tokenizer.pad_token_id] = -100
+        
+        return input_ids, attention_mask, encoded_targets
     
-def simple_accuracy(preds, labels):
-    return (preds == labels).mean().item()
 
-def acc_and_f1(preds, labels):
-    acc = simple_accuracy(preds, labels)
-    f1 = f1_score(y_true=labels, y_pred=preds).item()
-    return {
-        "accuracy": acc,
-        "f1": f1,
-    }
+
+
 
 def train(model, tokenizer, optimizer, train_set, validation_set, metric):
+    f1_score = load_metric('f1')
+    accuracy = load_metric('accuracy')
     # set training mode on the model
     model.train()
     
     # transfer model to cuda
     model.to('cuda')
     
-    epoch_train_loss = 0.
+    
+    
     for epoch in range(num_train_epochs):
+        epoch_train_loss = 0.
         epoch_total_example = 0
-        for input_ids,target_ids in tqdm(train_set):
+        for input_ids,maskeds_attention,target_ids in tqdm(train_set):
             epoch_total_example += input_ids.shape[0]
             optimizer.zero_grad()
             input_ids = input_ids.to('cuda')
             target_ids = target_ids.to('cuda')
-            outputs = model(input_ids=input_ids, labels=target_ids)
+            maskeds_attention = maskeds_attention.to('cuda')
+            outputs = model(input_ids=input_ids, attention_mask=maskeds_attention, labels=target_ids)
             loss = outputs.loss
             loss.backward()
             optimizer.step()
@@ -93,21 +111,23 @@ def train(model, tokenizer, optimizer, train_set, validation_set, metric):
         model.save_pretrained(f'results/{huggingface_model}/model/checkpoint-{epoch}')
         tokenizer.save_pretrained(f'results/{huggingface_model}/tokenizer/checkpoint-{epoch}')
     print("Training Ended")
-    print("Evaluate accuracy...")
-    model.eval()
-    f1, accuracy = 0., 0.
-    num_validation_set_examples = 0
-    for input_ids, target_ids in tqdm(validation_set):
-        model_predictions = model.generate(input_ids)
-        metric.add_batch(predictions=model_predictions, references=target_ids)
-        score = acc_and_f1(input_ids, target_ids)
-        f1 += score['f1']
-        accuracy += score['accuracy']
-        num_validation_set_examples += input_ids.shape[0]
-    final_score = metric.compute()
-    f1 = f1 / num_validation_set_examples
-    accuracy = accuracy / num_validation_set_examples
-    print(f"Sacreblue: {final_score:.4f}, \t F-1: {f1:.4f}, \t Simple Accuracy: {accuracy:.4f}")
+    # print("Evaluate accuracy...")
+    # model.eval()
+    # num_validation_set_examples = 0
+    # with torch.no_grad():
+    #     for input_ids, maskeds_attention, target_ids in tqdm(validation_set):
+    #         input_ids = input_ids.to('cuda')
+    #         target_ids = target_ids.to('cuda')
+    #         maskeds_attention = maskeds_attention.to('cuda')
+    #         model_predictions = model.generate(input_ids=input_ids, attention_mask=maskeds_attention,do_sample=False)
+    #         metric.add_batch(predictions=model_predictions, references=target_ids)
+    #         #accuracy.add_batch(predictions=model_predictions, references=target_ids)
+    #         #f1_score.add_batch(predictions=model_predictions, references=target_ids)
+    #         num_validation_set_examples += input_ids.shape[0]
+    # final_score = metric.compute()
+    # #f1 = f1_score.compute()
+    # #accuracy = accuracy.compute()
+    # print(f"Sacreblue: {final_score:.4f}")
     
 # Set seed
 set_seed(seed)
@@ -124,12 +144,14 @@ if __name__ == '__main__':
     # Define a metric for accuracy
     metric = load_metric('sacrebleu')
     
-    my_trainset = DataLoader(Dataset(_data["train"], tokenizer), batch_size=batch_size, num_workers=num_workers, collate_fn=lambda data: Dataset.pack_minibatch(data))
-    my_validation = Dataset(_data["validation"], tokenizer) 
+    _train_set = Dataset(_data["train"], tokenizer)
+    _validation_set = Dataset(_data["validation"], tokenizer)
+    my_trainset_dataloader = DataLoader(_train_set, batch_size=batch_size, num_workers=num_workers, collate_fn=lambda data: Dataset.pack_minibatch(data))
+    my_validation_dataloader = DataLoader(_validation_set, batch_size=batch_size, num_workers=num_workers, collate_fn=lambda data: Dataset.pack_minibatch(data))
     
     train(model = model,
           tokenizer = tokenizer,
           optimizer = optimizer, 
-          train_set = my_trainset,
-          validation_set = my_validation,
+          train_set = my_trainset_dataloader,
+          validation_set = my_validation_dataloader,
           metric = metric)
