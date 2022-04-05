85c6acf43163e6e0de8975b5047ddfa3b09f03d1
Giacomo Nunziati <72670358+nunziati@users.noreply.github.com>
2022-04-05 18:35:09 +0200

    Delete utils directory

--- utils/parse_duorc.py
+++
@@ -1,8 +0,0 @@
-def parse(example, remove_more_than_1_answer = True):
-    _inputs = []
-    _targets = []
-    for i in range(len(example['answers'])):
-        _inputs.append(f"question: {example['question']}  context: {example['plot']}")
-        _targets.append(example['answers'][i] if len(example['answers']) > 0 else "")
-        if remove_more_than_1_answer: break
-    return _inputs, _targets
